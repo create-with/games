bd472a497a7c9c7ec76c8012157bb800452b68d1
Bijan <bijanbwb@gmail.com>
2020-03-23 22:22:22 -0400

    Fix script issues and assets

--- client/index.js
+++ client/index.js
@@ -7,16 +7,6 @@ const app = Elm.Main.init({ node, flags });
 
 app.ports.playSound.subscribe(data => {
   console.log("Playing sound: " + data);
-
-  const soundPath = setSoundPath(data);
-  const sound = new Howl({src: [soundPath]});
-
+  const sound = new Howl({src: ["assets/sounds/" + data]});
   sound.play();
 });
-
-const setSoundPath = (name) => {
-  let soundPath = null;
-  if (name.includes("beep")) soundPath = "beep.413a14a4.wav"
-  if (name.includes("boop")) soundPath = "boop.bc3d69ea.wav"
-  return soundPath
-}
