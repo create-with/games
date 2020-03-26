3605a057df00f6d681f1ef003767615390952a07
Bijan <bijanbwb@gmail.com>
2020-03-26 19:51:15 -0400

    Prevent default keyboard behavior

--- client/index.js
+++ client/index.js
@@ -1,9 +1,13 @@
 import { Elm } from "./src/Main.elm";
 import { Howl } from "howler";
 
+// Elm Application
+
 const flags = {};
 const node = document.getElementById("elm-node");
 const app = Elm.Main.init({ node, flags });
+
+// Sounds with Howler
 
 app.ports.playSound.subscribe(data => {
   console.log("Playing sound: " + data);
@@ -15,3 +19,24 @@ app.ports.playSound.subscribe(data => {
 
   sound.play();
 });
+
+// Prevent Default Keyboard Behavior
+
+const gameKeys = {
+  " ": 32,
+  "ArrowUp": 38,
+  "ArrowDown": 40
+};
+
+const preventDefaultForGameKeys = (event) => {
+  const keys = Object.values(gameKeys)
+  if (keys.includes(event.keycode) ||
+    keys.includes(event.which))
+    event.preventDefault();
+}
+
+document.documentElement.addEventListener(
+  "keydown",
+  (event) => preventDefaultForGameKeys(event),
+  false
+);
