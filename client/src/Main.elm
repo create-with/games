f7e648fb87478745d13d0fb548ae5e85acb5bf28
Bijan <bijanbwb@gmail.com>
2020-04-29 21:54:01 -0400

    Comment out Breakout for now

--- client/src/Main.elm
+++ client/src/Main.elm
@@ -2,29 +2,32 @@ module Main exposing (main)
 
 -- IMPORTS
 
-import Breakout
+-- import Breakout
 import Browser
 import Pong
 
 
 
 -- MAIN
--- main : Program () Pong.Model Pong.Msg
--- main =
---     Browser.document
---         { init = Pong.init
---         , update = Pong.update
---         , subscriptions = Pong.subscriptions
---         , view = Pong.view
---         }
--- BREAKOUT
 
 
-main : Program () Breakout.Model Breakout.Msg
+main : Program () Pong.Model Pong.Msg
 main =
     Browser.document
-        { init = Breakout.init
-        , update = Breakout.update
-        , subscriptions = Breakout.subscriptions
-        , view = Breakout.view
+        { init = Pong.init
+        , update = Pong.update
+        , subscriptions = Pong.subscriptions
+        , view = Pong.view
         }
+
+
+
+-- BREAKOUT
+-- main : Program () Breakout.Model Breakout.Msg
+-- main =
+--     Browser.document
+--         { init = Breakout.init
+--         , update = Breakout.update
+--         , subscriptions = Breakout.subscriptions
+--         , view = Breakout.view
+--         }
