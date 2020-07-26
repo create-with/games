e97d1e19d9a1a6483b2efee11b3eddccc6286e84
Bijan <bijanbwb@gmail.com>
2020-07-26 18:57:40 -0400

    Starting on Breakout bricks

--- assets/elm/src/Breakout/Brick.elm
+++ assets/elm/src/Breakout/Brick.elm
@@ -24,8 +24,10 @@ type State
 
 type alias Brick =
     { color : String
+    , height : Float
     , position : Vector
     , state : State
+    , width : Float
     }
 
 
@@ -37,30 +39,46 @@ type alias Bricks =
 -- INIT
 
 
-initialBricks : List Brick
+initialBrick : Brick
+initialBrick =
+    { color = "white"
+    , height = 16
+    , position = ( 0, 0 )
+    , state = On
+    , width = 80
+    }
+
+
+initialBricks : Bricks
 initialBricks =
-    [ row "#F56565" 100
-    , row "#ED8936" 112
-    , row "#F6AD55" 124
-    , row "#ECC94B" 136
-    , row "#48BB78" 148
-    , row "#4299E1" 160
-    ] |> Util.List.flatten
-
-
-row : String -> Float -> List Brick
-row color verticalPosition =
-    [ Brick color ( 0, verticalPosition ) On
-    , Brick color ( 80, verticalPosition ) On
-    , Brick color ( 160, verticalPosition ) On
-    , Brick color ( 240, verticalPosition ) On
-    , Brick color ( 320, verticalPosition ) On
-    , Brick color ( 400, verticalPosition ) On
-    , Brick color ( 480, verticalPosition ) On
-    , Brick color ( 560, verticalPosition ) On
-    , Brick color ( 640, verticalPosition ) On
-    , Brick color ( 720, verticalPosition ) On
+    let
+        topOfFirstRow =
+            100
+    in
+    [ row "#F56565" (topOfFirstRow + (initialBrick.height * 0))
+    , row "#ED8936" (topOfFirstRow + (initialBrick.height * 1))
+    , row "#ECC94B" (topOfFirstRow + (initialBrick.height * 2))
+    , row "#48BB78" (topOfFirstRow + (initialBrick.height * 3))
+    , row "#4299E1" (topOfFirstRow + (initialBrick.height * 4))
+    , row "#667EEA" (topOfFirstRow + (initialBrick.height * 5))
     ]
+        |> Util.List.flatten
+
+
+row : String -> Float -> Bricks
+row color verticalPosition =
+    [ Brick color initialBrick.height ( initialBrick.width * 0, verticalPosition ) On initialBrick.width
+    , Brick color initialBrick.height ( initialBrick.width * 1, verticalPosition ) On initialBrick.width
+    , Brick color initialBrick.height ( initialBrick.width * 2, verticalPosition ) On initialBrick.width
+    , Brick color initialBrick.height ( initialBrick.width * 3, verticalPosition ) On initialBrick.width
+    , Brick color initialBrick.height ( initialBrick.width * 4, verticalPosition ) On initialBrick.width
+    , Brick color initialBrick.height ( initialBrick.width * 5, verticalPosition ) On initialBrick.width
+    , Brick color initialBrick.height ( initialBrick.width * 6, verticalPosition ) On initialBrick.width
+    , Brick color initialBrick.height ( initialBrick.width * 7, verticalPosition ) On initialBrick.width
+    , Brick color initialBrick.height ( initialBrick.width * 8, verticalPosition ) On initialBrick.width
+    , Brick color initialBrick.height ( initialBrick.width * 9, verticalPosition ) On initialBrick.width
+    ]
+
 
 
 -- VIEW
@@ -80,10 +98,13 @@ viewBrick brick =
     in
     Svg.rect
         [ Svg.Attributes.fill <| brick.color
+        , Svg.Attributes.fillOpacity "0.9"
         , Svg.Attributes.x <| String.fromFloat x
         , Svg.Attributes.y <| String.fromFloat y
         , Svg.Attributes.width <| "80"
-        , Svg.Attributes.height <| "12"
+        , Svg.Attributes.height <| "16"
+        , Svg.Attributes.stroke "white"
+        , Svg.Attributes.strokeWidth "1"
+        , Svg.Attributes.strokeOpacity "0.5"
         ]
         []
-
