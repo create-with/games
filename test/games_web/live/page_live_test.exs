d59e844df67aabb4bbd0f4a85b93d993bca7c2b9
Bijan <bijanbwb@gmail.com>
2020-08-15 13:00:22 -0400

    Passing tests

--- test/games_web/live/page_live_test.exs
+++ test/games_web/live/page_live_test.exs
@@ -5,7 +5,7 @@ defmodule GamesWeb.PageLiveTest do
 
   test "disconnected and connected render", %{conn: conn} do
     {:ok, page_live, disconnected_html} = live(conn, "/")
-    assert disconnected_html =~ "Welcome to Phoenix!"
-    assert render(page_live) =~ "Welcome to Phoenix!"
+    assert disconnected_html =~ "div id=\"elm\""
+    assert render(page_live) =~ "div id=\"elm\""
   end
 end
